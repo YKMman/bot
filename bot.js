1a8c2071bdfcda5a44076be23aee9f5f17138b8c
Kirill <ykirillm@mail.ru>
2024-08-02 02:21:42 +0300

    add project

---
+++ bot.js
@@ -0,0 +1,29 @@
+const TelegramBot = require('node-telegram-bot-api');
+const dotenv = require('dotenv').config()
+const webAppUrl = 'https://31.129.45.49:4173/';
+
+const bot = new TelegramBot(process.env.API_KEY_BOT, {
+    polling: true
+});
+
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
+    if(text === '/start') {
+        await bot.sendMessage(chatId, 'Заходите в наше приложение', {
+            reply_markup: {
+                inline_keyboard: [
+                    [{text: 'Открыть приложение', web_app: {url: webAppUrl}}]
+                ]
+            }
+        })
+    } else {
+        await bot.sendMessage(chatId, 'Вы зашли в наше приложение?', {
+            reply_markup: {
+                inline_keyboard: [
+                    [{text: 'Открыть приложение', web_app: {url: webAppUrl}}]
+                ]
+            }
+        })
+    }
+});
